ddd02d3dc5abbe114d6819d6400308078a83a2fa
Joe Kucera <joekucera2002@gmail.com>
2015-02-05 20:38:42 -0700

    Implementing Web Entry (Work in progress)

---
+++ PayrollManagment/PayrollManagement.Web/Models/Dependent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayrollManagement.Web.Models
+{
+    public class Dependent
+    {
+        public Int32 Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public Int16 Age { get; set; }
+    }
+}
---
+++ PayrollManagment/PayrollManagement.Web/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayrollManagement.Web.Models
+{
+    public class Employee
+    {
+        public Int32 Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public ICollection<Dependent> Dependents { get; set; }
+    }
+}
